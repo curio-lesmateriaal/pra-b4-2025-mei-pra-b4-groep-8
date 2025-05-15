fea2fdf9f1c574dc053c76a50bc35cb663d60e94
Meand05 <djay.verlaar@icloud.com>
2025-05-15 12:08:51 +0200

    A2 Af

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -14,16 +14,12 @@ namespace PRA_B4_FOTOKIOSK.controller
         // De window die we laten zien op het scherm
         public static Home Window { get; set; }
 
-
         // De lijst met fotos die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
-        
-        
+
         // Start methode die wordt aangeroepen wanneer de foto pagina opent.
         public void Start()
         {
-
-
             PicturesToDisplay.Clear();
 
             // Initializeer de lijst met fotos
@@ -31,28 +27,40 @@ namespace PRA_B4_FOTOKIOSK.controller
             // foreach is een for-loop die door een array loopt
             foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
             {
-                var now  =DateTime.Now;
+                var now = DateTime.Now;
                 int dayToday = (int)now.DayOfWeek;
 
                 string folderName = Path.GetFileName(dir); // bijv. "0_Zondag"
                 string[] parts = folderName.Split('_');
 
-
                 if (parts.Length > 0 && int.TryParse(parts[0], out int folderDay) && folderDay == dayToday)
-                
-
+                {
                     /**
                      *
                      * dir string is de map waar de fotos in staan. Bijvoorbeeld:
                      * \fotos\0_Zondag
                      */
                     foreach (string file in Directory.GetFiles(dir))
-                {
-                    /**
-                     * file string is de file van de foto. Bijvoorbeeld:
-                     * \fotos\0_Zondag\10_05_30_id8824.jpg
-                     */
-                    PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                    {
+                        string fileName = Path.GetFileName(file); // bijv. "10_05_30_id8824.jpg"
+                        string[] fileParts = fileName.Split('_');
+
+                        if (fileParts.Length > 3)
+                        {
+                            int hour = int.Parse(fileParts[0]);
+                            int minute = int.Parse(fileParts[1]);
+                            int second = int.Parse(fileParts[2]);
+
+                            DateTime fileDateTime = new DateTime(now.Year, now.Month, now.Day, hour, minute, second);
+
+                            TimeSpan timeDifference = now - fileDateTime;
+
+                            if (timeDifference.TotalMinutes <= 20 && timeDifference.TotalMinutes >= 2)
+                            {
+                                PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                            }
+                        }
+                    }
                 }
             }
 
@@ -65,6 +73,5 @@ namespace PRA_B4_FOTOKIOSK.controller
         {
 
         }
-
     }
 }
